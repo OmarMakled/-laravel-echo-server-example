8a787ca5292c88ae7adc4df9023de62230477f6b
Omar <omar.makled@gmail.com>
2021-02-13 19:43:20 +0200

    readme

--- readme.md
+++ readme.md
@@ -1,3 +1,45 @@
 ## Laravel-echo-server
 
-TODO
+Before installation laravel valet and redis shoud be up and running.
+
+## Server Side
+
+```
+ git clone git@github.com:OmarMakled/laravel-echo-server-example.git
+ cd laravel-echo-server-example
+ composer install
+ cp .env.example .env
+```
+
+Update `.env` file
+
+```
+APP_URL=laravel-echo-server-example.test
+BROADCAST_DRIVER=redis
+QUEUE_CONNECTION=redis
+MIX_APP_URL="${APP_URL}"
+```
+
+```
+php artisan key:generate
+php artisan optimize
+```
+
+## Client side
+
+```
+npm i
+./node_modules/.bin/laravel-echo-server init
+./node_modules/.bin/laravel-echo-server start
+npm run watch
+```
+
+## Emit an event
+
+```
+php artisan queue:work
+php artisan tinker
+event(new \App\Events\UserEvent());
+```
+
+Enjoy !
--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,23 +1,23 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">Socket.io Component</div>
 
-                    <div class="card-body">I'm an example component.</div>
-                </div>
-            </div>
+          <div class="card-body">Open the console.</div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    mounted() {
-        window.Echo.channel("messages").listen(".UserEvent", function(data) {
-            console.log(data);
-        });
-    }
+  mounted() {
+    window.Echo.channel("messages").listen(".UserEvent", function (data) {
+      console.log(data);
+    });
+  },
 };
 </script>
